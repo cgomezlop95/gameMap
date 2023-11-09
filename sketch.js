d3673b24677534c54f660c2e9f8867a6f2b2acd1
Cristina <c.gomezlop95@gmail.com>
2023-11-09 12:42:05 +0100

    game over and restart functionality

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
 let buildingsAdded = [];
-let canvasWidth = 900;
+let canvasWidth = 1100; //Anterior: 900
 let canvasHeigth = 600;
-let lifeUpdate = 100;
+
+let hungerUpdate = 100;
+let hygieneUpdate = 100;
+let socialUpdate = 100;
+
 let numberOfConstructions = 0;
 let dialogueText = "";
 let dialogueVisible = false;
 let fontSize = 25;
+
+//Game over functionality
+let gameOver = false;
+let restartButton;
 
 function preload() {
   backgroundImageTest = loadImage("./imgs/backgroundTest1600.png");
@@ -22,100 +30,121 @@ function preload() {
 function setup() {
   firstPlayer = new Player(50, 50, playerImg);
   merchant = new Merchant(50, 400, merchantImg);
-  boat = new Boat(1400, 750, boatImg);
+  boat = new Boat(400, 800, boatImg);
   backgroundTest = new backgroundImage(0, 0, backgroundImageTest);
   moneyIcon = new Money(700, 400, moneyImg);
   createCanvas(canvasWidth, canvasHeigth);
+  //Game over functionality:
+  restartButton = createButton("Restart");
+  restartButton.position(width / 2 - 50, height / 2);
+  restartButton.hide();
+  restartButton.mousePressed(startNewGame);
 }
 
 function buildTower() {
-  const newTower = new Tower(firstPlayer.x, firstPlayer.y, towerImg);
+  const newTower = new Tower(firstPlayer.x - 110, firstPlayer.y, towerImg);
   buildingsAdded.push(newTower);
   updateConstructions();
 }
 
 function buildHouse() {
-  const newHouse = new House(firstPlayer.x, firstPlayer.y, houseImg);
+  const newHouse = new House(firstPlayer.x - 110, firstPlayer.y, houseImg);
   buildingsAdded.push(newHouse);
   updateConstructions();
 }
 
 function buildCastle() {
-  const newCastle = new Castle(firstPlayer.x, firstPlayer.y, castleImg);
+  const newCastle = new Castle(firstPlayer.x - 110, firstPlayer.y, castleImg);
   buildingsAdded.push(newCastle);
   updateConstructions();
 }
 
 function buildBar() {
-  const newBar = new Bar(firstPlayer.x, firstPlayer.y, tavernImg);
+  const newBar = new Bar(firstPlayer.x - 210, firstPlayer.y, tavernImg);
   buildingsAdded.push(newBar);
   updateConstructions();
 }
 
 function draw() {
   backgroundTest.draw();
-  firstPlayer.update();
-  firstPlayer.draw();
-  merchant.draw();
-  boat.draw();
-  for (let i = 0; i < buildingsAdded.length; i++) {
-    buildingsAdded[i].draw();
-  }
-  //MOVIMIENTO POR LOS 4 LADOS//
-  if (firstPlayer.x >= canvasWidth - 200) {
-    firstPlayer.x = canvasWidth - 200;
-    if (
-      backgroundTest.x >= canvasWidth - backgroundImageTest.width &&
-      keyIsDown(RIGHT_ARROW)
-    ) {
-      backgroundTest.x -= 5;
-      merchant.x -= 5;
-      for (let i = 0; i < buildingsAdded.length; i++) {
-        buildingsAdded[i].x -= 5;
+  if (!gameOver) {
+    firstPlayer.update();
+    firstPlayer.draw();
+    merchant.draw();
+    boat.draw();
+    for (let i = 0; i < buildingsAdded.length; i++) {
+      buildingsAdded[i].draw();
+    }
+    //MOVIMIENTO POR LOS 4 LADOS//
+    if (firstPlayer.x >= canvasWidth - 200) {
+      firstPlayer.x = canvasWidth - 200;
+      if (
+        backgroundTest.x >= canvasWidth - backgroundImageTest.width &&
+        keyIsDown(RIGHT_ARROW)
+      ) {
+        backgroundTest.x -= 5;
+        merchant.x -= 5;
+        boat.x -= 5;
+        for (let i = 0; i < buildingsAdded.length; i++) {
+          buildingsAdded[i].x -= 5;
+        }
       }
     }
-  }
 
-  if (firstPlayer.x < 10) {
-    firstPlayer.x = 10;
-    if (backgroundTest.x < 0 && keyIsDown(LEFT_ARROW)) {
-      backgroundTest.x += 5;
-      merchant.x += 5;
-      for (let i = 0; i < buildingsAdded.length; i++) {
-        buildingsAdded[i].x += 5;
+    if (firstPlayer.x < 10) {
+      firstPlayer.x = 10;
+      if (backgroundTest.x < 0 && keyIsDown(LEFT_ARROW)) {
+        backgroundTest.x += 5;
+        merchant.x += 5;
+        boat.x += 5;
+        for (let i = 0; i < buildingsAdded.length; i++) {
+          buildingsAdded[i].x += 5;
+        }
       }
     }
-  }
 
-  if (firstPlayer.y >= canvasHeigth - 200) {
-    firstPlayer.y = canvasHeigth - 200;
-    if (
-      backgroundTest.y >= canvasHeigth - backgroundImageTest.height &&
-      keyIsDown(DOWN_ARROW)
-    ) {
-      backgroundTest.y -= 5;
-      merchant.y -= 5;
-      for (let i = 0; i < buildingsAdded.length; i++) {
-        buildingsAdded[i].y -= 5;
+    if (firstPlayer.y >= canvasHeigth - 200) {
+      firstPlayer.y = canvasHeigth - 200;
+      if (
+        backgroundTest.y >= canvasHeigth - backgroundImageTest.height &&
+        keyIsDown(DOWN_ARROW)
+      ) {
+        backgroundTest.y -= 5;
+        merchant.y -= 5;
+        boat.y -= 5;
+        for (let i = 0; i < buildingsAdded.length; i++) {
+          buildingsAdded[i].y -= 5;
+        }
       }
     }
-  }
 
-  if (firstPlayer.y < 10) {
-    firstPlayer.y = 10;
-    if (backgroundTest.y < 0 && keyIsDown(UP_ARROW)) {
-      backgroundTest.y += 5;
-      merchant.y += 5;
-      for (let i = 0; i < buildingsAdded.length; i++) {
-        buildingsAdded[i].y += 5;
+    if (firstPlayer.y < 10) {
+      firstPlayer.y = 10;
+      if (backgroundTest.y < 0 && keyIsDown(UP_ARROW)) {
+        backgroundTest.y += 5;
+        merchant.y += 5;
+        boat.y += 5;
+        for (let i = 0; i < buildingsAdded.length; i++) {
+          buildingsAdded[i].y += 5;
+        }
       }
     }
-  }
-  //MOVIMIENTO POR LOS 4 LADOS//
+    //MOVIMIENTO POR LOS 4 LADOS//
 
-  if (dialogueVisible) {
-    textSize(fontSize); // Set the font size
-    text(dialogueText, canvasWidth / 2, canvasHeigth / 2);
+    if (dialogueVisible) {
+      textSize(fontSize); // Set the font size
+      text(dialogueText, 300, 300);
+    }
+
+    if (hungerUpdate <= 0 || hygieneUpdate <= 0 || socialUpdate <= 0) {
+      gameOver = true;
+    }
+  } else {
+    // Display the "Game Over" message when the game is over
+    fill(255);
+    textSize(40);
+    text("Game Over", 200, 300);
+    restartButton.show();
   }
 }
 
@@ -126,10 +155,16 @@ function updateConstructions() {
 }
 
 setInterval(function () {
-  console.log("Este mensaje se repetirá cada 10 segundo.");
-  lifeUpdate -= 10;
-  const life = document.getElementById("life");
-  life.innerHTML = `${lifeUpdate}`;
+  //console.log("Este mensaje se repetirá cada 10 segundo.");
+  hungerUpdate -= 10;
+  const hunger = document.getElementById("hunger");
+  hunger.innerHTML = `${hungerUpdate}`;
+  hygieneUpdate -= 10;
+  const hygiene = document.getElementById("hygiene");
+  hygiene.innerHTML = `${hygieneUpdate}`;
+  socialUpdate -= 10;
+  const social = document.getElementById("social");
+  social.innerHTML = `${socialUpdate}`;
 }, 10000);
 
 function mouseMoved() {
@@ -143,9 +178,9 @@ function mouseMoved() {
     // The mouse was clicked on the image
     // You can add your code here to handle the image click event
     //console.log("Image clicked!");
-    const dialogue = document.getElementById("dialogueMerchant");
-    dialogue.innerHTML = "Hola, Milburn, ¿cómo puedo ayudarte?";
-    dialogueText = "Hola, Milburn, ¿cómo puedo ayudarte?";
+    //const dialogue = document.getElementById("dialogueMerchant");
+    //dialogue.innerHTML = "Hola, Milburn, ve al puerto a pescar";
+    dialogueText = "Hola, Milburn, deberías ir al puerto a pescar";
     dialogueVisible = true;
   } else if (
     mouseX >= boat.x &&
@@ -156,12 +191,14 @@ function mouseMoved() {
     // The mouse was clicked on the image
     // You can add your code here to handle the image click event
     //console.log("Image clicked!");
-    dialogueText = "Haz clic para pescar";
+    dialogueText = "Haz clic en el barco para recoger la pesca";
     dialogueVisible = true;
   } else {
     dialogueVisible = false;
   }
 }
+
+//¿Cómo recuperar hunger, hygiene y social, al hacer clic en elementos?
 
 function mousePressed() {
   if (
@@ -170,6 +207,17 @@ function mousePressed() {
     mouseY >= boat.y &&
     mouseY <= boat.y + boatImg.height
   ) {
-    console.log("Pesca");
+    //console.log("Pesca");
+    hungerUpdate += 100; //Al hacer clic, gano 100 puntos en vida/alimento
+    hunger.innerHTML = `${hungerUpdate}`;
   }
+}
+
+function startNewGame() {
+  hungerUpdate = 100;
+  hygieneUpdate = 100;
+  socialUpdate = 100;
+  gameOver = false;
+  restartButton.hide();
+  clear();
 }
